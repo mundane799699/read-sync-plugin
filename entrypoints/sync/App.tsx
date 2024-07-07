35688ec0470aa40d6a10746338ac005238597706
mundane <799699348@qq.com>
2024-07-07 23:06:08 +0800

    集成tailwindcss

--- entrypoints/sync/App.tsx
+++ entrypoints/sync/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>同步读书笔记</h1>
+      <h1 className="text-lg font-bold underline text-green-500">
+        同步读书笔记
+      </h1>
     </>
   );
 }
